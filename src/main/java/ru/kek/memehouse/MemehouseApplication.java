e26efc8eb3c557271818643c2388d2d2d8e7f8fe
gordeevnm <gordeevnm@gmail.com>
2017-10-05 21:04:56 +0300

    init

---
+++ src/main/java/ru/kek/memehouse/MemehouseApplication.java
@@ -0,0 +1,12 @@
+package ru.kek.memehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemehouseApplication.class, args);
+	}
+}
