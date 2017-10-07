5dbd2cd52a88ee1988813acffa0b48d50475e132
gordeevnm <gordeevnm@gmail.com>
2017-10-08 01:24:30 +0300

    сделал контекст спринга public static полем

--- src/main/java/ru/kek/memehouse/MemeHouseApplication.java
+++ src/main/java/ru/kek/memehouse/MemeHouseApplication.java
@@ -3,11 +3,13 @@ package ru.kek.memehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MemeHouseApplication {
+	public static ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemeHouseApplication.class, args);
+		applicationContext = SpringApplication.run(MemeHouseApplication.class, args);
 	}
 }
