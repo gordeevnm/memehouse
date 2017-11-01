875589a80fd9e5939a876a21ab7bf9607037520b
gordeevnm <gordeevnm@gmail.com>
2017-11-01 08:47:20 +0300

    добавил флаг для автоматической авторизации тестовым токеном(от админа); починил авторизацию(редирект на /)

--- src/main/java/ru/kek/memehouse/configuration/security/TokenAuthenticationFilter.java
+++ src/main/java/ru/kek/memehouse/configuration/security/TokenAuthenticationFilter.java
@@ -4,10 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.kek.memehouse.configuration.security.exceptions.AuthException;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +20,8 @@ import java.io.IOException;
  * 06.10.17
  */
 public class TokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+	private static final boolean TEST_MODE = true;
+	private static final String TEST_TOKEN = "TEST_TOKEN";
 
 	TokenAuthenticationFilter(AuthenticationManager authenticationManager) {
 		super(new AntPathRequestMatcher("/api/**"));
@@ -37,10 +41,23 @@ public class TokenAuthenticationFilter extends AbstractAuthenticationProcessingF
 		String tokenValue = request.getHeader("X-Auth-Token");
 
 		if (isNullOrEmpty(tokenValue)) {
-			throw new AuthException("Authentication token is not found");
-		} else {
-			return getAuthenticationManager().authenticate(new AuthenticationToken(tokenValue));
+			if (TEST_MODE)
+				tokenValue = TEST_TOKEN;
+			else
+				throw new AuthException("Authentication token is not found");
 		}
+
+		return getAuthenticationManager().authenticate(new AuthenticationToken(tokenValue));
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Authentication success. Updating SecurityContextHolder to contain: "
+					+ authResult);
+		}
+		SecurityContextHolder.getContext().setAuthentication(authResult);
+		chain.doFilter(request, response);
 	}
 
 	@Override
