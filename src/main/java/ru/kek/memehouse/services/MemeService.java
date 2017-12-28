74a4db57b6c24476c4bcfacfba2abe5d3f94e0c9
gordeevnm <gordeevnm@gmail.com>
2017-12-29 00:21:30 +0300

    создал классы для сервисов

--- src/main/java/ru/kek/memehouse/services/MemeService.java
+++ src/main/java/ru/kek/memehouse/services/MemeService.java
@@ -4,18 +4,23 @@ import ru.kek.memehouse.configuration.security.AuthenticationToken;
 import ru.kek.memehouse.models.Meme;
 import ru.kek.memehouse.models.Tag;
 
+import java.sql.Timestamp;
 import java.util.List; /**
  * gordeevnm@gmail.com
  * 07.10.17
  */
 public interface MemeService {
-	List<Meme> search(String query, String name, List<Tag> tags, int count, int offset, AuthenticationToken token);
+	List<Meme> search(String query, String type, String visibility, List<Tag> tags, Timestamp periodStart, Timestamp periodEnd, int count, int offset, AuthenticationToken token);
 
 	Meme add(Meme meme, AuthenticationToken token);
 
-	Meme get(long memeId, AuthenticationToken token);
+	Meme get(int memeId, AuthenticationToken token);
 
-	Meme edit(Meme meme, AuthenticationToken token);
+	Meme edit(int memeId, Meme meme, AuthenticationToken token);
 
-	void delete(long memeId, AuthenticationToken token);
+	void delete(int memeId, AuthenticationToken token);
+
+	void save(int memeId, AuthenticationToken token);
+
+	void addNote(int memeId, String note, AuthenticationToken token);
 }
